cedc7603e5463cebe283fc730f0b9da776ddaa06
Eric Liu <ericyl.us@gmail.com>
2021-02-03 15:10:29 -0800

    chore(deps-dev): use generate from svg-to-svelte@2.1

--- build.js
+++ build.js
@@ -1,15 +1,1 @@
-const fs = require("fs");
-const { generateFromFolder, generateIndex } = require("svg-to-svelte");
-const { name: pkgName, devDependencies } = require("./package.json");
-
-(async () => {
-  const lib = "bootstrap-icons";
-  const { moduleNames } = await generateFromFolder(`node_modules/${lib}/icons`);
-
-  await generateIndex({
-    moduleNames,
-    pkgName,
-    pkgVersion: devDependencies[lib],
-    outputFile: "ICON_INDEX.md",
-  });
-})();
+require("svg-to-svelte").generate("node_modules/bootstrap-icons/icons");
