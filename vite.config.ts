60a8f926ad02dbea18ddadd131dc919a159554eb
Eric Liu <ericyl.us@gmail.com>
2023-12-31 09:02:01 -0800

    ci: add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
-import { ghPages } from "vite-plugin-gh-pages";
 
 export default defineConfig({
-  plugins: [svelte(), ghPages()],
+  plugins: [svelte()],
 });
